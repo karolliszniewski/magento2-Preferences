8dc764627f9d9407e1a4d05de7d3f9399b36d536
Karol Liszniewski <105976690+karolliszniewski@users.noreply.github.com>
2024-11-06 14:48:59 +0000

    Add files via upload

---
+++ FixReports/Controller/Adminhtml/Report/Product/ExportSoldExcel.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Aware\FixReports\Controller\Adminhtml\Report\Product;
+
+
+use Magento\Backend\Block\Widget\Grid\ExportInterface;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\App\Filesystem\DirectoryList;
+
+class ExportSoldExcel extends \Magento\Reports\Controller\Adminhtml\Report\Product
+{
+    /**
+     * Authorization level of a basic admin session
+     *
+     * @see _isAllowed()
+     */
+    const ADMIN_RESOURCE = 'Magento_Reports::report_products';
+
+    /**
+     * Export Sold Products report to XML format action
+     *
+     * @return ResponseInterface
+     */
+    public function execute()
+    {
+        $this->_view->loadLayout();
+        $fileName = 'products_ordered.xml';
+        /** @var ExportInterface $exportBlock */
+        $exportBlock = $this->_view->getLayout()->getChildBlock('adminhtml.report.grid', 'grid.export');
+        return $this->_fileFactory->create(
+            $fileName,
+            $exportBlock->getExcelFile($fileName),
+            DirectoryList::VAR_DIR
+        );
+    }
+}
---
+++ FixReports/registration.php
@@ -0,0 +1,5 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Aware_FixReports', __DIR__);
